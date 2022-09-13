3779293b80b943a355c893f0ab05135d2537e82a
sangwoneum <sweum@surromind.ai>
2022-09-13 16:56:57 +0900

    commit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220913.143850';
+const cacheName = 'chirpy-20220913.155357';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
